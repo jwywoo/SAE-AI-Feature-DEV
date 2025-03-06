e2135795c0bbb953cda100603e5ada2aed1898be
jwywoo <jwywoo26@gmail.com>
2025-03-06 15:08:44 +0900

    [FEAT] #14 Locust Setting
    - Locust: load test done -> 충격과 공포
    - API for collecting test case implemented

--- SAE-API/src/stock/router.py
+++ SAE-API/src/stock/router.py
@@ -1,6 +1,7 @@
+from http.client import HTTPException
 from fastapi import APIRouter, Depends, status
 from stock.schemas import StockGenerationRequestDto, StockGenerationResponseDto
-from stock.service import stock_generation_service
+from stock.service import stock_generation_service, test_case_collector_service
 
 router = APIRouter()
 
@@ -12,3 +13,10 @@ router = APIRouter()
 )
 async def stock_generation_router(request: StockGenerationRequestDto):
     return await stock_generation_service(request)
+
+
+@router.post("/ai/stock/test", status_code=status.HTTP_201_CREATED)
+async def test_case_collector_router(request: StockGenerationRequestDto):
+    if await test_case_collector_service(request):
+        return {"message": "Test case saved successfully."}
+    raise HTTPException(status_code=500, detail="Failed to save test case.")
